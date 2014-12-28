792f977c15301ec00bb46bbaae1ba0e29c4f62ef
kailashrdave <kailashrdave@gmail.com>
2014-12-28 15:50:42 +0100

    Bill class added
    Bill class added

---
+++ app/src/main/java/com/catchblocker/mutualpay/backend/Entites/Bill.java
@@ -0,0 +1,18 @@
+package com.catchblocker.mutualpay.backend.Entites;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by public on 12/28/2014.
+ */
+public class Bill {
+    private UUID ID;
+    private Date BillDate;
+    private double Amount;
+    private int PaidByMember; //
+    private String Remark;
+    private String BillImagePath;
+    private String PaymentMode;
+
+}
