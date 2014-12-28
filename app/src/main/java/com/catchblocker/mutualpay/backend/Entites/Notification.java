d3219f1d9bb2827a92d8ec42b732ccce7e4e32b9
kailashrdave <kailashrdave@gmail.com>
2014-12-28 15:54:55 +0100

    Notification class added
    Notification class Added for managing notifications

---
+++ app/src/main/java/com/catchblocker/mutualpay/backend/Entites/Notification.java
@@ -0,0 +1,14 @@
+package com.catchblocker.mutualpay.backend.Entites;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by public on 12/28/2014.
+ */
+public class Notification {
+    public UUID ID;
+    public Date NotificationDate;
+    public String Text;
+    public int Initiator;
+}
